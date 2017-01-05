5ae12ea825d2dc7b3436a6e2d90dec7b67f99f3a
cfdobber <cfrisod@gmail.com>
2017-01-05 09:38:59 +0100

    Add initial jigsaw project with maven

---
+++ fd-java9-jigsaw/fd-java9-jigsaw-cli/src/main/java/nl/frisodobber/java9/jigsaw/calculator/cli/Main.java
@@ -0,0 +1,11 @@
+package nl.frisodobber.java9.jigsaw.calculator.cli;
+
+/**
+ * Created by dobber on 31-12-16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("You can use the following functions: ");
+    }
+}
---
+++ fd-java9-jigsaw/fd-java9-jigsaw-gui/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+/**
+ * Created by dobber on 31-12-16.
+ */
+module calculator.gui {
+    requires calculator.algorithm.api;
+    uses nl.frisodobber.java9.jigsaw.calculator.algorithm.api.Algorithm;
+}
